8052ed6ff60fdf951fe1ecb13c6a7626a656fa98
raul <raul.argemi.7e6@itb.cat>
2023-11-23 16:43:59 +0100

    Register / Gmail

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../connection');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+
 
 router.get('/users/user', async (req,res) =>{
   try {
@@ -24,10 +27,26 @@ router.get('/users', async (req, res) => {
     res.status(500).json({ error: 'Error al obtener datos de usuarios' });
   }
 });
-//REGISTRO
+
+// Configura el transporter de nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'Gmail', 
+  auth: {
+    user: 'rasbiosport@gmail.com',
+    pass: 'rasbio1432',
+  },
+});
+
+// Función para generar el token de verificación
+function generateVerificationToken() {
+  return crypto.randomBytes(20).toString('hex');
+}
+
+// RUTA DE REGISTRO
 router.post('/register', async (req, res) => {
   const { nombre, telefono, correo, contraseña, confirmarContraseña } = req.body;
 
+  // Validaciones
   if (!nombre || !telefono || !correo || !contraseña || !confirmarContraseña) {
     return res.status(400).json({ error: 'Todos los campos son obligatorios' });
   }
@@ -41,26 +60,37 @@ router.post('/register', async (req, res) => {
 
     if (existingUser) {
       return res.status(409).json({ error: 'El correo ya está registrado' });
-    } 
-  } catch (error) {
-    console.error('Hola Error en el registro:', error);
-    res.status(500).json({ error: 'Error en el registro. Inténtalo de nuevo más tarde.' });
-  }
+    }
 
-  try{
+    const verificationToken = generateVerificationToken();
+
+    const mailOptions = {
+      from: 'rasbiosport@gmail.com',
+      to: correo,
+      subject: 'Verificación de registro',
+      text: `¡Gracias por registrarte en tu aplicación! Haz clic en el siguiente enlace para verificar tu correo: http://localhost:8080/login/${verificationToken}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error al enviar el correo de verificación:', error);
+        return res.status(500).json({ error: 'Error al enviar el correo de verificación' });
+      }
+
+      console.log('Correo de verificación enviado:', info.response);
+    });
+
+    // Resto de la lógica de registro
     const nextUserId = await db.one('SELECT nextval(\'user_id_seq\')');
-    
     await db.none('INSERT INTO usuari (id_user, name_user, user_phone, user_email, user_password) VALUES ($1, $2, $3, $4, $5)', [nextUserId.nextval, nombre, telefono, correo, contraseña]);
 
     res.status(200).json({ message: 'Registro exitoso' });
-  } catch(error){
-    console.log('Prueba')
+  } catch (error) {
+    console.error('Error en el registro:', error);
+    res.status(500).json({ error: 'Error en el registro. Inténtalo de nuevo más tarde.' });
   }
+});
 
-  }
-
-
-);
 
 //LOGIN
 const SECRET_KEY = '1243';
