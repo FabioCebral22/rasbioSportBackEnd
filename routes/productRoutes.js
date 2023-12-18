161a9e1779685137a93627fbaa1720c3c69ebf92
Fabio <fabio.cebral.7e5@itb.cat>
2023-12-18 15:50:29 +0100

    Editar Productos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,33 +12,26 @@ router.get('/products', async (req, res) => {
     `;
     const params = [];
     const conditions = [];
-
     if (req.query.category_name) {
       conditions.push('category.category_name = $' + (params.length + 1));
       params.push(req.query.category_name);
     }
-
     if (req.query.size) {
       conditions.push('product_size = $' + (params.length + 1));
       params.push(req.query.size);
     }
-
     if (req.query.min_price && req.query.max_price) {
       conditions.push('product_price BETWEEN $' + (params.length + 1) + ' AND $' + (params.length + 2));
       params.push(req.query.min_price);
       params.push(req.query.max_price);
     }
-
     if (conditions.length) {
       query += ' WHERE ' + conditions.join(' AND ');
     }
-
     console.log('Query SQL:', query);
     console.log('Parámetros:', params);
-
     const products = await db.any(query, params);
     console.log('Productos encontrados:', products);
-
     res.json(products);
   } catch (error) {
     console.error('Error al obtener los productos:', error);
@@ -46,26 +39,7 @@ router.get('/products', async (req, res) => {
   }
 });
 
-//AÑADIR PRODUCTO
-// router.post('/addProduct', async (req, res) => {
-//   const { nombreProducto, categoriaProducto, infoProducto, contraseña, imgProducto, tagProducto, tallaProducto } = req.body;
 
-//   if (!nombreProducto || !categoriaProducto || !infoProducto || !contraseña || !imgProducto || !tagProducto || !tallaProducto) {
-//     return res.status(400).json({ error: 'Todos los campos son obligatorios' });
-//   }
-
-//   try {
-//     const categoryIdResult = await db.one('SELECT category_id FROM category WHERE category_name = $1', [categoriaProducto]);
-
-//     const nextProductId = await db.one('SELECT nextval(\'product_id_seq\')');
-//     await db.none('INSERT INTO product (product_name, category_id, product_desc, product_info, product_image, product_size, product_prize, product_tag) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [nombreProducto, categoryIdResult.category_id, null, infoProducto, imgProducto, tallaProducto, null, tagProducto]);
-
-//     res.status(200).json({ message: 'Registro exitoso' });
-//   } catch (error) {
-//     console.error('Error:', error);
-//     res.status(500).json({ error: 'Error en el servidor' });
-//   }
-// });
 
 
 router.delete('/products/:id', async (req, res) => {
@@ -106,7 +80,32 @@ router.post('/products', async (req, res) => {
     res.status(500).json({ error: 'Error al añadir el producto' });
   }
 });
+router.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const {
+    product_name,
+    category_id,
+    product_desc,
+    product_info,
+    product_image,
+    product_size,
+    product_price,
+    product_tag
+  } = req.body;
 
+  try {
+    console.log("hola")
+    await db.none(
+      'UPDATE product SET product_name = $1, category_id = $2, product_desc = $3, product_info = $4, product_image = $5, product_size = $6, product_price = $7, product_tag = $8 WHERE product_id = $9',
+      [product_name, category_id, product_desc, product_info, product_image, product_size, product_price, product_tag, id]
+    );
+
+    res.status(200).json({ message: 'Producto actualizado exitosamente' });
+  } catch (error) {
+    console.error('Error al actualizar el producto:', error);
+    res.status(500).json({ error: 'Error al actualizar el producto' });
+  }
+});
 
 
 const authMiddleware = require('../authMiddleware');
